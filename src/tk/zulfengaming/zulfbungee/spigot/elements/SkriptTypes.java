8349fc86bf9bd82b744d7610ab02db8dae6b7e88
zulfen <passjosh04@gmail.com>
2022-05-18 14:14:44 +0100

    Alpha v0.7.0 - Made a slight adjustment to line up with Skript API changes.

--- src/tk/zulfengaming/zulfbungee/spigot/elements/SkriptTypes.java
+++ src/tk/zulfengaming/zulfbungee/spigot/elements/SkriptTypes.java
@@ -5,6 +5,7 @@ import ch.njol.skript.classes.Parser;
 import ch.njol.skript.expressions.base.EventValueExpression;
 import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.registrations.Classes;
+import org.jetbrains.annotations.NotNull;
 import tk.zulfengaming.zulfbungee.universal.util.skript.ProxyPlayer;
 import tk.zulfengaming.zulfbungee.universal.util.skript.ProxyServer;
 
@@ -19,29 +20,25 @@ public class SkriptTypes {
                 .parser(new Parser<ProxyPlayer>() {
 
                     @Override
-                    public ProxyPlayer parse(String s, ParseContext context) {
+                    public ProxyPlayer parse(@NotNull String s, @NotNull ParseContext context) {
                         return null;
                     }
 
                     @Override
-                    public boolean canParse(ParseContext context) {
+                    public boolean canParse(@NotNull ParseContext context) {
                         return false;
                     }
 
                     @Override
-                    public String toString(ProxyPlayer proxyPlayer, int i) {
+                    public @NotNull String toString(ProxyPlayer proxyPlayer, int i) {
                         return proxyPlayer.getName();
                     }
 
                     @Override
-                    public String toVariableNameString(ProxyPlayer proxyPlayer) {
+                    public @NotNull String toVariableNameString(ProxyPlayer proxyPlayer) {
                         return proxyPlayer.getName();
                     }
 
-                    @Override
-                    public String getVariableNamePattern() {
-                        return "\\s+";
-                    }
                 }));
 
         Classes.registerClass(new ClassInfo<>(ProxyServer.class, "proxyserver")
@@ -52,29 +49,25 @@ public class SkriptTypes {
                 .parser(new Parser<ProxyServer>() {
 
                     @Override
-                    public ProxyServer parse(String s, ParseContext context) {
+                    public ProxyServer parse(@NotNull String s, @NotNull ParseContext context) {
                         return null;
                     }
 
                     @Override
-                    public boolean canParse(ParseContext context) {
+                    public boolean canParse(@NotNull ParseContext context) {
                         return false;
                     }
 
                     @Override
-                    public String toString(ProxyServer proxyServer, int i) {
+                    public @NotNull String toString(ProxyServer proxyServer, int i) {
                         return proxyServer.getName();
                     }
 
                     @Override
-                    public String toVariableNameString(ProxyServer proxyServer) {
+                    public @NotNull String toVariableNameString(ProxyServer proxyServer) {
                         return proxyServer.getName();
                     }
 
-                    @Override
-                    public String getVariableNamePattern() {
-                        return "\\s+";
-                    }
                 }));
     }
 }
