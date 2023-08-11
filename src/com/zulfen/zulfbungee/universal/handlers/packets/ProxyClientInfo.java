e59389d956e0cccf8b1ee829369faecab18e8fc8
passj <passjosh04@gmail.com>
2023-08-11 11:25:00 +0100

    Alpha v0.9.9 - Fixes to potential invalid config errors.

--- src/com/zulfen/zulfbungee/universal/handlers/packets/ProxyClientInfo.java
+++ src/com/zulfen/zulfbungee/universal/handlers/packets/ProxyClientInfo.java
@@ -37,19 +37,8 @@ public class ProxyClientInfo<P, T> extends PacketHandler<P, T> {
                 boolean addressCheck = infoSockAddr.getAddress().equals(socketAddressIn.getAddress());
 
                 if (addressCheck && portCheck) {
-
                     getMainServer().addActiveConnection(connection, name, clientInfo);
                     return new Packet(PacketTypes.CONNECTION_NAME, true, true, name);
-
-                } else {
-
-                    getProxy().warning(String.format("ZulfBungee couldn't find the client with the address %s!", socketAddressIn.getAddress()));
-                    getProxy().warning("Please make sure that the address in your proxy's main config is valid!");
-                    getProxy().warning(String.format("Address check returned %s (%s compared to %s)", false, infoSockAddr.getAddress(), socketAddressIn.getAddress()));
-                    getProxy().warning(String.format("Port check returned %s (%s compared to %s)", portCheck, infoSockAddr.getPort(), clientInfo.getMinecraftPort()));
-                    getProxy().warning("If you are sure this server is in your config, either provide a connection name or register it with ZulfBungee syntax and restart that server.");
-                    return new Packet(PacketTypes.INVALID_CONFIGURATION, true, true);
-
                 }
 
             }
